3375c0bef8bddee291168f551c27f4ca78951714
TonyBHerrera <tonybherrera@yahoo.com>
2024-12-03 21:54:21 -0700

    clocking 60fps

--- main.py
+++ main.py
@@ -7,18 +7,25 @@ import constants
 screen_width = constants.SCREEN_WIDTH
 screen_height = constants.SCREEN_HEIGHT
 running = True
+
 def main():
-  print("Starting asteroids!")
-  print(f"Screen width: {constants.SCREEN_WIDTH}")
-  print(f"Screen height: {constants.SCREEN_HEIGHT}")
+  
   pygame.init()
   screen = pygame.display.set_mode((screen_width, screen_height))
+  
+  clock = pygame.time.Clock()
+  dt = 0
+
+
   while running:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
     screen.fill((0,0,0))
     pygame.display.flip()
+
+  dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
